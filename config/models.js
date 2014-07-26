f7f623116c917d46bc03bb32053a823eb3ccef1d
Marcelo Boeira <marceloboeira@live.com>
2014-07-26 13:32:20 -0300

    Preset attributes...

--- config/models.js
+++ config/models.js
@@ -2,4 +2,12 @@
 module.exports.models = {
 
   connection: process.env.ENV || 'development',
+
+  attributes: {
+  	_sid: {
+  		type: 'string',
+  		required:true,
+  		unique: true
+  	}
+  }
 };
